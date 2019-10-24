70bd1cf9274cdb8684bc14c3a6ecbbacd78cf3bd
Doncho Minkov <DonchoMinkov@gmail.com>
2019-10-24 08:53:27 +0300

    Fixed zoo's solution

--- classes_and_objects/zoos.py
+++ classes_and_objects/zoos.py
@@ -17,15 +17,29 @@ class Zoo:
             animals = self.birds
         return animals
 
+    def get_species_plural(self, singular):
+        if singular == 'mammal':
+            return 'Mammals'
+        elif singular == 'fish':
+            return 'Fishes'
+        else:
+            return 'Birds'
+
     def add_animal(self, species, animal):
         animals = self.get_animals_by_species(species)
         animals.append(animal)
         self.__animals += 1
 
-    def get_info(self, species):
+    def get_info_by_species(self, species):
         animals = self.get_animals_by_species(species)
-        print(animals)
-        print(self.__animals)
+        return f'{self.get_species_plural(species)} in {self.name}: {", ".join(animals)}'
+
+    def get_info_animals(self):
+        return f'Total animals: {self.__animals}'
+
+    def get_info(self, species):
+        return self.get_info_by_species(species) + '\n' + \
+               self.get_info_animals()
 
 
 zoo = Zoo(input())
@@ -38,4 +52,4 @@ for _ in range(animals_count):
 
 species = input()
 
-zoo.get_info(species)
+print(zoo.get_info(species))
